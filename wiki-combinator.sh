ed8249147f6fc82576fdcb0349dd27779429f25d
John <jm786@njit.edu>
2019-06-17 09:37:52 -0400

    Update wiki-combinator.sh

--- wiki-combinator.sh
+++ wiki-combinator.sh
@@ -2,15 +2,9 @@
 
 if [ $# -eq 1 ]
 then
-<<<<<<< HEAD
 	processors=$1
 else
-	processors=10
-=======
-        processors=$1
-else
         processors=10
->>>>>>> 289b7d2efede768c2c9a686092ac8d6233350dd3
 fi
 
 echo "Creating lists for all dumps in the folder"
@@ -19,32 +13,17 @@ list_path=$master_path/wikilists
 
 if [ -d $list_path ]
 then
-<<<<<<< HEAD
-	echo "Creation of the Wikilists directory failed"
-else
-	mkdir $list_path
-	echo "Wikilists directory created successfully"
-=======
         echo "Creation of the Wikilists directory failed"
 else
         mkdir $list_path
         echo "Wikilists directory created successfully"
->>>>>>> 289b7d2efede768c2c9a686092ac8d6233350dd3
 fi
 
 dump_path=$master_path/wikidumps
 
 if [ -d $dump_path ]
 then
-<<<<<<< HEAD
-	echo "Creation of the Wikidumps directory failed"
-else
-	mkdir $dump_path
-	echo "Wikiidumps directory created successfully"
-
-=======
         echo "Creation of the Wikidumps directory failed"
 else
         mkdir $dump_path
         echo "Wikiidumps directory created successfully"
->>>>>>> 289b7d2efede768c2c9a686092ac8d6233350dd3
